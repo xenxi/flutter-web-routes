852d3d167ee886d11842be82e414972df362f2f3
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-13 19:05:41 +0100

    :lipstick: add navbar buttons

---
+++ lib/presentation/shared/custom_elevated_button.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class CustomTextButton extends StatelessWidget {
+  const CustomTextButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+  final String text;
+  final void Function() onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: onPressed, child: Text(text));
+  }
+}
--- lib/presentation/shared/custom_navbar_menu.dart
+++ lib/presentation/shared/custom_navbar_menu.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_routes_spike/presentation/shared/custom_elevated_button.dart';
 
 class CustumNavbarMenu extends StatelessWidget {
   const CustumNavbarMenu({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 50,
-      height: 50,
-      color: Colors.red,
+    return Row(
+      children: [
+        CustomTextButton(text: 'Page 1', onPressed: () {}),
+        CustomTextButton(text: 'Page 2', onPressed: () {}),
+      ],
     );
   }
 }
