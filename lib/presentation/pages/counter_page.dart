4ae9921c5fadfb84909ce54a532f1b2d30c3e0e7
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-13 18:27:37 +0100

    :lipstick: add increment btn

--- lib/presentation/pages/counter_page.dart
+++ lib/presentation/pages/counter_page.dart
@@ -3,11 +3,33 @@ import 'package:flutter/material.dart';
 class CounterPage extends StatelessWidget {
   static String route = 'counter';
   const CounterPage({Key? key}) : super(key: key);
-
+  final numOfClicks = 0;
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Hello World'),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              'Counter: $numOfClicks',
+              style: Theme.of(context).textTheme.headline2,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(), padding: const EdgeInsets.all(30)),
+            child: const Icon(
+              Icons.add,
+              size: 35,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
     );
   }
 }
