6b0841e01160f16feb4ef746844649c510493eec
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-13 14:14:21 +0100

    :art: improve structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_routes_spike/presentation/router_app.dart';
 
-import 'presentation/pages/counter_page.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Router Spike',
-      initialRoute: CounterPage.route,
-      routes: {CounterPage.route: (_) => const CounterPage()},
-    );
-  }
-}
+void main() => runApp(const RouterApp());
---
+++ lib/presentation/router_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_routes_spike/presentation/pages/counter_page.dart';
+
+class RouterApp extends StatelessWidget {
+  const RouterApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Router Spike',
+      initialRoute: CounterPage.route,
+      routes: {CounterPage.route: (_) => const CounterPage()},
+    );
+  }
+}
