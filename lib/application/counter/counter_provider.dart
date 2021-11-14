0194199ac76de7201b00e94b3713d3f9cbcb9a36
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-14 21:00:21 +0100

    :bug: fix provider

--- lib/application/counter/counter_provider.dart
+++ lib/application/counter/counter_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CounterProvider extends ChangeNotifier {
   int _counter = 0;
--- lib/presentation/pages/provider_counter_page.dart
+++ lib/presentation/pages/provider_counter_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_web_routes_spike/application/counter/counter_provider.dart';
 import 'package:flutter_web_routes_spike/presentation/shared/custom_navbar_menu.dart';
@@ -12,35 +14,46 @@ class ProviderCounterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => CounterProvider(),
-      child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CustumNavbarMenu(),
-            const Spacer(),
-            _buildText(context),
-            const SizedBox(
-              height: 20,
-            ),
-            _buildActions(context),
-            const Spacer(),
-          ],
-        ),
+      child: const _BuildPageBody(),
+    );
+  }
+}
+
+class _BuildPageBody extends StatelessWidget {
+  const _BuildPageBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<CounterProvider>(context);
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CustumNavbarMenu(),
+          const Spacer(),
+          _buildText(context, counter: provider.counter),
+          const SizedBox(
+            height: 20,
+          ),
+          _buildActions(provider),
+          const Spacer(),
+        ],
       ),
     );
   }
 
-  Widget _buildText(BuildContext context) => Center(
+  Widget _buildText(BuildContext context, {required int counter}) => Center(
         child: FittedBox(
           fit: BoxFit.contain,
           child: Text(
-            'Counter: ${Provider.of<CounterProvider>(context).counter}',
+            'Counter: $counter',
             style: Theme.of(context).textTheme.headline2,
           ),
         ),
       );
 
-  Widget _buildActions(BuildContext context) => FittedBox(
+  Widget _buildActions(CounterProvider provider) => FittedBox(
         fit: BoxFit.cover,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -48,14 +61,12 @@ class ProviderCounterPage extends StatelessWidget {
             CustomCircleButton(
               iconData: Icons.remove,
               color: Colors.red,
-              onPressed: () =>
-                  Provider.of<CounterProvider>(context).decrement(),
+              onPressed: () => provider.decrement(),
             ),
             CustomCircleButton(
               iconData: Icons.add,
               color: Colors.green,
-              onPressed: () =>
-                  Provider.of<CounterProvider>(context).increment(),
+              onPressed: () => provider.increment(),
             ),
           ],
         ),
