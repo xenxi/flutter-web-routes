5c06d391be854e37c933f1648b259a27121152d1
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-14 20:30:23 +0100

    :dizzy: add mobile page transition

--- lib/routerGenerator.dart
+++ lib/routerGenerator.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_web_routes_spike/presentation/pages/counter_page.dart';
 
 import 'presentation/pages/not_found_page.dart';
@@ -15,10 +16,29 @@ class RouteGenerator {
         _routesMap[settings.name]?.call() ?? const NotFoundPage();
 
     return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => FadeTransition(
-        opacity: animation,
-        child: searchPage,
-      ),
+      pageBuilder: (context, animation, secondaryAnimation) => searchPage,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        if (kIsWeb) {
+          return _buildWebTransition(animation, searchPage);
+        }
+
+        return _buildMobileTransition(
+            animation, secondaryAnimation, searchPage);
+      },
     );
   }
+
+  Widget _buildMobileTransition(Animation<double> animation,
+          Animation<double> secondaryAnimation, Widget searchPage) =>
+      CupertinoPageTransition(
+          primaryRouteAnimation: animation,
+          secondaryRouteAnimation: secondaryAnimation,
+          child: searchPage,
+          linearTransition: true);
+
+  Widget _buildWebTransition(Animation<double> animation, Widget searchPage) =>
+      FadeTransition(
+        opacity: animation,
+        child: searchPage,
+      );
 }
